d323a891ea849496faed3a87624c7024deaf1b5c
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 17:59:06 +0100

    added util to process input before saving

--- server/model/user-model.js
+++ server/model/user-model.js
@@ -8,3 +8,18 @@ const userSchema = new mongoose.Schema({
     last: String,
     password: String,
 });
+
+userSchema.set("timestamps", true);
+
+
+
+userSchema.pre("save", function(next) {
+    this.username = this.username.toLowerCase();
+    this.first = this.first.toLowerCase();
+    this.last = this.last.toLowerCase();
+    const unsafePassword = this.password;
+    this.password = bcrypt.hashSync(unsafePassword);
+    next();
+});
+
+export default mongoose.model("user", userSchema);
