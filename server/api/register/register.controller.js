28573ec2c47746e858f4dac480b8026a5175226b
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 18:05:16 +0100

    added username checker

--- server/api/register/register.controller.js
+++ server/api/register/register.controller.js
@@ -14,4 +14,15 @@ export function index(req, res) {
         first: req.body.first,
         last: req.body.last,
     });
+
+    user.save((error) => {
+        if (error) {
+            // Mongoose Error Code 11000 means that the username is taken (validation error)
+            if (error.code === 11000) {
+                return res.status(403).json({ message: "Username is already taken" });
+            }
+            return res.status(500).json();
+        }
+        return res.status(201).json();
+    });
 }
