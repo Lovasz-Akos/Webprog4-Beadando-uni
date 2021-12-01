0c98daa75698ef34049437a3ec3dd940c760d885
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 18:18:17 +0100

    added task creation function

--- server/api/task/tasks-controller.js
+++ server/api/task/tasks-controller.js
@@ -11,3 +11,22 @@ export function index(req, res) {
         return res.status(200).json({ tasks: tasks });
     }).populate("author", "username", "user");
 }
+
+export function create(req, res) {
+    const id = auth.getUserId(req);
+    User.findOne({ _id: id }, (error, user) => {
+        if (error && !user) {
+            return res.status(500).json();
+        }
+        const task = new Task(req.body.task);
+        task.author = user._id;
+        task.dueDate = moment(task.dueDate);
+
+        task.save((error) => {
+            if (error) {
+                return res.status(500).json();
+            }
+            return res.status(201).json();
+        });
+    });
+}
