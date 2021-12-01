a0626019dc5bda26b8d742785be6c772d0df5742
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 18:43:34 +0100

    added login checker

--- server/api/auth/auth-controller.js
+++ server/api/auth/auth-controller.js
@@ -3,5 +3,29 @@ import User from "../../model/user-model";
 import { generateJWT } from "../../services/auth-service";
 
 export function index(req, res) {
+    const validation = validateIndex(req.body);
 
+    if (!validation.isValid) {
+        return res.status(400).json({ message: validation.message });
+    }
+
+    User.findOne({ username: req.body.username.toLowerCase() }, (error, user) => {
+        if (error) {
+            return res.status(500).json();
+        }
+
+        if (!user) {
+            return res.status(401).json();
+        }
+
+        const passwordsMatch = User.passwordMatches(
+            req.body.password,
+            user.password
+        );
+        if (!passwordsMatch) {
+            return res.status(401).json();
+        }
+        const token = generateJWT(user);
+        return res.status(200).json({ token: token });
+    });
 }
