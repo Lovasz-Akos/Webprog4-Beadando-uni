34f99ca12794999083ed0d432a531ba8fa87b4be
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 18:38:37 +0100

    added login requrement and token decoder

--- server/services/auth-service.js
+++ server/services/auth-service.js
@@ -21,3 +21,28 @@ export function getUserId(req) {
     }
     return token.user.id;
 }
+
+export function requireLogin(req, res, next) {
+    const token = decodeToken(req);
+    if (!token) {
+        return res.status(401).json({ message: "You must be logged in." });
+    }
+    next();
+}
+
+/**
+ * @param {HTTP Request} req
+ */
+export function decodeToken(req) {
+    const token = req.headers.authorization || req.headers["authorization"];
+
+    if (!token) {
+        return null;
+    }
+
+    try {
+        return jwt.verify(token, process.env.TOKEN_SECRET);
+    } catch (error) {
+        return null;
+    }
+}
