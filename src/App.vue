7b83d329c5a4cfec44803aacd062e88c2b54b575
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 17:34:59 +0100

    replaced template home page and added footer/navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <Navbar />
+    <div id="app-container">
+      <router-view/>
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import Navbar from '@/components/Navbar.vue'
+  import Footer from '@/components/Footer.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      Navbar,
+      Footer
+    },
+    beforeCreate: function() {
+      // Ensure that on app boot, we check if the user is authenticated or not
+      this.$store.dispatch('authenticate');
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
