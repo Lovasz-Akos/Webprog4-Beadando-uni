88d636afa869598854b590ce70430901c83abf4f
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 18:15:02 +0100

    added reg form validation

--- server/api/register/register.controller.js
+++ server/api/register/register.controller.js
@@ -26,3 +26,25 @@ export function index(req, res) {
         return res.status(201).json();
     });
 }
+
+function validateIndex(body) {
+    let errors = "";
+
+    if (StringUtil.isEmpty(body.username)) {
+        errors += "Username is required. ";
+    }
+    if (StringUtil.isEmpty(body.password)) {
+        errors += "Password is required. ";
+    }
+    if (StringUtil.isEmpty(body.first)) {
+        errors += "First name is required. ";
+    }
+    if (StringUtil.isEmpty(body.last)) {
+        errors += "Last name is required. ";
+    }
+
+    return {
+        isValid: StringUtil.isEmpty(errors),
+        message: errors,
+    };
+}
