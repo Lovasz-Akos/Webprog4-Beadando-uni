d81590ceefc2890b0b460524dd69c5fdd5f8ee10
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 18:40:21 +0100

    added auth-controller

---
+++ server/api/auth/auth-controller.js
@@ -0,0 +1,7 @@
+import { StringUtil } from "../../utilities/string-util";
+import User from "../../model/user-model";
+import { generateJWT } from "../../services/auth-service";
+
+export function index(req, res) {
+
+}
