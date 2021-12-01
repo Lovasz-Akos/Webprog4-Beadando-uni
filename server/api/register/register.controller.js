5dffd7276b065d9f5026bb7de3d11056e74af99a
Lovász Ákos <akos4x44@gmail.com>
2021-12-01 18:03:11 +0100

    added validation error check to start of reg.cntrl

--- server/api/register/register.controller.js
+++ server/api/register/register.controller.js
@@ -1,4 +1,10 @@
 import { StringUtil } from "../../utilities/string-util";
 import User from "../../model/user-model";
 
-export function index(req, res) {}
+export function index(req, res) {
+    const validation = validateIndex(req.body);
+
+    if (!validation.isValid) {
+        return res.status(400).json({ message: validation.message });
+    }
+}
