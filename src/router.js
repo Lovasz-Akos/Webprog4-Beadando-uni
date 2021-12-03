7204650429d0ce282fba7795d16253301e3b4c51
Lovász Ákos <akos4x44@gmail.com>
2021-12-03 09:50:29 +0100

    added view routing

--- src/router.js
+++ src/router.js
@@ -3,7 +3,65 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 export default new Router({
-    routes: [],
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/tasks',
+            name: 'tasks-all',
+            component: TasksAll,
+            beforeEnter: (to, from, next) => {
+                //needs login to be accessed
+                if (auth.isLoggedIn()) {
+                    next();
+                } else {
+                    next('/login');
+                }
+            }
+        },
+        {
+            path: '/tasks/new',
+            name: 'tasks-create',
+            component: TasksCreate,
+            beforeEnter: (to, from, next) => {
+                if (auth.isLoggedIn()) {
+                    next();
+                } else {
+                    next('/login');
+                }
+            }
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register,
+            beforeEnter: (to, from, next) => {
+                if (!auth.isLoggedIn()) {
+                    next();
+                } else {
+                    next('/');
+                }
+            }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+            beforeEnter: (to, from, next) => {
+                if (!auth.isLoggedIn()) {
+                    next();
+                } else {
+                    next('/');
+                }
+            }
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+    ],
     linkActiveClass: 'active',
     mode: 'history'
 })
