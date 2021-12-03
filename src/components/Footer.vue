fc7dcba729fb5625538343d91942b692d384535a
Lovász Ákos <akos4x44@gmail.com>
2021-12-03 13:56:39 +0100

    footer style adjustment

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer id="custom-footer">
-    <span class="mr-4">&copy; 2021 Arman Abkar</span>
+    <span class="mr-4">&copy; Lovász Ákos - FMNUMU - 🍕</span>
     <span class="mr-4">Privacy</span>
     <span>Terms of Service</span>
   </footer>
