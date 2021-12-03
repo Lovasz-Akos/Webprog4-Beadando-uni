c2546617245b0b7d7ad23cfea5a1cc19fc5f54bb
Lovász Ákos <akos4x44@gmail.com>
2021-12-03 11:12:17 +0100

    added allowing of cross origin request

--- server/config/env.js
+++ server/config/env.js
@@ -23,10 +23,10 @@ export function setEnvironment(app) {
  */
 function setDevEnv(app) {
     process.env.NODE_ENV = "development";
-    app.use(bodyParser.json()); // Allows parsing JSON from the client
-    app.use(morgan("dev")); // Log HTTP Requests to the node console (for debugging purposes)
+    app.use(bodyParser.json());
+    app.use(morgan("dev"));
     app.use(cors()); // Enable Cross Origin Requests, since Vue.JS is on a different origin
-    process.env.DB_URL = "mongodb://localhost:27017/mevn-task-manager";
+    process.env.DB_URL = "mongodb://localhost:27017/mevnTSK";
     process.env.TOKEN_SECRET = "27946982a9a4408996607fe95a6299f9";
 }
 
@@ -37,8 +37,8 @@ function setDevEnv(app) {
 function setProdEnv(app) {
     process.env.NODE_ENV = "production";
     process.env.DB_URL =
-        "mongodb://user:password123@ds133642.mlab.com:33642/vue-db";
-    process.env.TOKEN_SECRET = "9f4d50bfa6b2450a9ec766fc5104948e";
+        "mongodb://localhost:27017/mevnTSK";
+    process.env.TOKEN_SECRET = "27946982a9a4408996607fe95a6299f9";
     app.use(bodyParser.json());
     app.use(express.static(__dirname + "/../../dist"));
 }
